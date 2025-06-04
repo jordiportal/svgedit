f5a1bdd15762d882e155885c132015cb759f236f
Jordi Portal <jordiportal@khlloreda.com>
2025-06-04 12:13:27 +0200

    fix: Añadir archivo de traducción en español faltante para ext-connector - Resuelve error 404 al cargar es.js - Traducciones para herramienta de conexión de objetos

---
+++ src/editor/extensions/ext-connector/locale/es.js
@@ -0,0 +1,12 @@
+export default {
+  name: 'Conector',
+  langListTitle: 'Conectar dos objetos',
+  langList: [
+    { id: 'mode_connect', title: 'Conectar dos objetos' }
+  ],
+  buttons: [
+    {
+      title: 'Conectar dos objetos'
+    }
+  ]
+} 
